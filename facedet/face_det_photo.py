41f7c10690ff343b134795184753a76a8d1ca25c
malharpatel2498 <53108424+malharpatel2498@users.noreply.github.com>
2019-07-30 10:03:30 +0530

    Update face_det_photo.py

--- facedet/face_det_photo.py
+++ facedet/face_det_photo.py
@@ -4,7 +4,7 @@ from PIL import Image
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img = cv2.imread('test8.jpg') //please insert the path of the image on your system
+img = cv2.imread('test8.jpg') #please insert the path of the image on your system
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
